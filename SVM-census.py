99f3499d76684386361e48d8e72709de09df3c47
Praveen Kumar <72409023+prvnktech@users.noreply.github.com>
2021-09-05 11:45:00 +0530

    Add files via upload

---
+++ SVM-census.py
@@ -0,0 +1,160 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# 1
+
+
+import pandas as pd
+import numpy as np
+
+# 2
+
+
+
+original_data =pd.read_csv(
+    "adult.csv",
+    names=[
+        "Age","Workclass","fnlwgt","Education","Education-Num","Marital Status",
+        "Occupation","Relationship","Race","Gender","Capital Gain","Capital Loss",
+        "Hours per week","Country","Target"],
+    sep=r'\s*,\s*',
+    engine='python',
+    na_values='?')
+original_data.head()
+
+
+# 3
+
+
+import matplotlib.pyplot as plt
+import math
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+fig = plt.figure(figsize=(20,20))
+cols=3
+rows=math.ceil(float(original_data.shape[1])/cols)
+
+for i,column in enumerate(['Age','Workclass','Education','Occupation','Race','Gender']):
+    ax=fig.add_subplot(rows,cols,i+1)
+    ax.set_title(column)
+    if original_data.dtypes[column] ==np.object: 
+        original_data[column].value_counts().plot(kind="bar",axes=ax)
+    else:
+        original_data[column].hist(axes=ax)
+        plt.xticks(rotation='vertical')
+plt.subplots_adjust(hspace=0.7, wspace=0.2)
+plt.show()
+
+
+
+# 4
+
+import sklearn.preprocessing as preprocessing
+le=preprocessing.LabelEncoder()
+original_data['Occupation']=le.fit_transform(original_data['Occupation'].astype(str))
+original_data.head()
+
+
+# 5
+
+
+original_data['Target']=le.fit_transform(original_data['Target'].astype(str))
+original_data.tail()
+
+
+# 6
+
+original_data.groupby('Education-Num').Target.mean().plot(kind='bar')
+plt.show()
+
+# 7
+
+
+
+from sklearn.model_selection import train_test_split
+X=original_data[['Education-Num','Occupation']]
+Y=original_data['Target']
+X_train,x_test,Y_train,y_test=train_test_split(X,Y,test_size=0.2,random_state=0)
+
+# 8
+
+
+
+from sklearn.svm import SVC
+classifier=SVC()
+classifier.fit(X_train,Y_train)
+score=classifier.score(x_test,y_test)
+print(score)
+
+
+# 9
+
+
+import seaborn as sns
+corrmat=original_data.corr()
+f,ax=plt.subplots(figsize=(7,7))
+sns.heatmap(corrmat,vmax=.8, square=True);
+plt.show()
+
+
+# 10
+
+
+original_data['Race']=le.fit_transform(original_data['Race'].astype(str))
+original_data['Gender']=le.fit_transform(original_data['Gender'].astype(str))
+original_data['Marital Status']=le.fit_transform(original_data['Marital Status'].astype(str))
+original_data['Education']=le.fit_transform(original_data['Education'].astype(str))
+
+
+# 11
+
+
+corrmat=original_data.corr()
+f,ax=plt.subplots(figsize=(7,7))
+sns.heatmap(corrmat,vmax=.8, square=True);
+plt.show()
+
+
+# 12
+
+
+f,ax=plt.subplots(figsize=(7,7))
+sns.heatmap(corrmat,vmax=.8,square=True,annot=True,fmt='.2f')
+plt.show()
+
+# 13
+
+
+
+X=original_data[['Education-Num','Occupation','Age','Gender']]
+Y=original_data['Target']
+X_train,x_test,Y_train,y_test=train_test_split(X,Y,test_size=0.2,random_state=0)
+classifier=SVC()
+classifier.fit(X_train,Y_train)
+score=classifier.score(x_test,y_test)
+print(score)
+
+
+# 14
+
+
+classifier =SVC(kernel='rbf', C=1.0)
+classifier.fit(X_train,Y_train)
+score=classifier.score(x_test,y_test)
+print(score)
+
+
+# 15
+
+
+classifier =SVC(kernel='linear', C=10.0)
+classifier.fit(X_train,Y_train)
+score=classifier.score(x_test,y_test)
+print(score)
+
+
+
+
+
+
+
